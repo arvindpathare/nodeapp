243c8af1e62f4d219bb717f120a576f719c15bf9
Arvind Pathare <arvindpathare111@gmail.com>
2019-08-18 15:35:52 +0530

    changes are done

--- script/deploy.bash
+++ script/deploy.bash
@@ -1,5 +1,5 @@
 #!/bin/sh
-ssh ec2-user@52.66.201.253 <<EOF
+ssh -T ec2-user@52.66.201.253 <<EOF
     cd ~/node-app
     git pull origin master
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
