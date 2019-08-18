1be0717de800b7e3374cd1f4bf13965a65a79809
Arvind Pathare <arvindpathare111@gmail.com>
2019-08-18 16:33:26 +0530

    testing

--- script/deploy.bash
+++ script/deploy.bash
@@ -1,9 +1,9 @@
 #!/bin/sh
 ssh -T ec2-user@52.66.201.253 <<EOF
-    cd /node-app
+    cd ~/node-app
 	yum install git -y
     git pull origin master
-    curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
+    curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
     . ~/.nvm/nvm.sh
     nvm install v10.11.0
     npm install
